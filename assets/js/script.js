62a6b646449f34cf2cb2e4843881d364434e67da
Jonathan Cano <jcano528@gmail.com>
2021-07-02 02:08:32 -0500

    add ability to add task

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+var buttonEl = document.querySelector("#save-task");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+
+/* buttonEl.addEventListener("click", function() {
+    var listItemEl = document.createElement("li"); // create a new task item
+    listItemEl.className = "task-item"; // style the new task item
+    listItemEl.textContent = "This is a new task."; // add the text
+    tasksToDoEl.appendChild(listItemEl); // append this element to the task list
+}); The next two functions are this function split into two */
+
+// this is the function that will run when the user clicks on the button...
+var createTaskHandler = function() {
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent = "This is a new task.";
+    tasksToDoEl.appendChild(listItemEl);
+}
+
+// and this is the function that calls it as a callback using an event listener
+buttonEl.addEventListener("click", createTaskHandler);
